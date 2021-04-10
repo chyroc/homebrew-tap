c3a23b0a9a1874569b18cc713e16ddac8652a52e
chyroc <chyroc@qq.com>
2021-04-11 02:25:23 +0800

    fix

--- Formula/dropbox-save-url.rb
+++ Formula/dropbox-save-url.rb
@@ -5,7 +5,7 @@ class DropboxSaveUrl < Formula
   desc "save url to dropbox"
   homepage "https://github.com/chyroc/dropbox-save-url"
   url "https://github.com/chyroc/dropbox-save-url/releases/download/v0.1.0/dropbox-save-url-0.1.1.tar.gz"
-  sha256 "ed9c51608e212fc67e94c1b1db461688b01f89ebd3f60d75a82528adbce3ee70"
+  sha256 "a2bdb1b551cd605b97d7f20078b61e7f2a5821765d7fe2e1ca18eed3e023d6f0"
   license "Apache-2.0"
   head "https://github.com/chyroc/dropbox-save-url"
 
