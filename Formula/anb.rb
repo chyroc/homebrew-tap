2ad8942c1bfb02eaa230435c47cc33e8c0cac428
chyroc <chyroc@qq.com>
2022-10-20 19:52:31 +0800

    update anb

--- Formula/anb.rb
+++ Formula/anb.rb
@@ -1,17 +1,11 @@
-# frozen_string_literal: true
-
-# brew config for anb
 class Anb < Formula
-  desc "tool to operator server"
+  desc "部署服务"
   homepage "https://github.com/chyroc/anb"
-  url "https://github.com/chyroc/anb/releases/download/v0.1.0/anb-0.1.0.tar.gz"
-  sha256 "7ea434ad685da14acc4cce57b814e21702787370e701381764ed1b5393d6587b"
-  license "Apache-2.0"
-  head "https://github.com/chyroc/anb"
+  head "https://github.com/chyroc/anb.git", branch: "master"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args
+    system "go", "build", *std_go_args(output: bin/"anb", ldflags: "-s -w")
   end
 end
